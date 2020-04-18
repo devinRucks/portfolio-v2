5b4f75e878b5aecf06ad401dbe5061ca2274184e
Devin Rucks <blindnives@gmail.com>
2020-04-18 11:15:50 -0400

    Added animation between routes

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,19 +1,30 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, withRouter } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import Home from './Home.js'
 import Project from './Project'
 import '../styling/container.scss'
 
-const Container = () => {
+const Container = ({ location }) => {
      return (
           <div id="content-container">
-               <Switch>
-                    <Route exact path="/" component={() => <Home />} />
-                    <Route path="/projects" component={() => <Project />} />
-               </Switch>
+               <TransitionGroup className="transition-group">
+                    <CSSTransition
+                         key={location.key}
+                         timeout={{ enter: 300, exit: 300 }}
+                         classNames={'fade'}
+                    >
+                         <section id="route-section">
+                              <Switch location={location}>
+                                   <Route exact path="/" component={Home} />
+                                   <Route path="/projects" component={Project} />
+                              </Switch>
+                         </section>
+                    </CSSTransition>
+               </TransitionGroup>
           </div>
      );
 }
 
 
-export default Container;
+export default withRouter(Container);
