a7afd79b0e3a852c88bff83497db68f707918dfa
Devin Rucks <blindnives@gmail.com>
2020-04-22 17:33:03 -0400

    added animation to home page heading text

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,17 +1,27 @@
 import React, { useState, useEffect, useRef } from 'react'
 import RINGS from 'vanta/dist/vanta.rings.min'
 import { CSSTransition } from "react-transition-group";
+import Fade from 'react-reveal/Fade';
 import * as THREE from 'three'
 import '../styling/home.scss'
 
 const Home = (props) => {
 	const [vantaEffect, setVantaEffect] = useState(0)
 	const [mounted, setMount] = useState(false)
+	const [showTopHeading, setTopHeading] = useState(false)
+	const [showMidHeading, setMidHeading] = useState(false)
+	const [showBottomHeading, setBottomHeading] = useState(false)
 	const myRef = useRef(null)
 
 	useEffect(() => {
 		setMount(true)
 	}, [mounted])
+
+	useEffect(() => {
+		setTimeout(() => setTopHeading(true), 1000)
+		setTimeout(() => setMidHeading(true), 1000)
+		setTimeout(() => setBottomHeading(true), 1000)
+	}, [showTopHeading, showMidHeading, showBottomHeading])
 
 	useEffect(() => {
 		if (!vantaEffect) {
@@ -32,8 +42,19 @@ const Home = (props) => {
 		<CSSTransition in={mounted} timeout={1200} classNames="home">
 			<div ref={myRef} id="foreground-content">
 				<div id="heading-container">
-					<div className="heading">Devin Rucks</div>
-					<div className="sub-heading">Software Developer</div>
+
+					<Fade left opposite when={showTopHeading}>
+						<div className="top-heading" >Hi, my name is</div>
+					</Fade>
+
+					<Fade left opposite when={showMidHeading}>
+						<div className="mid-heading">Devin Rucks.</div>
+					</Fade>
+
+					<Fade left opposite when={showBottomHeading}>
+						<div className="bottom-heading">I'm a Software Developer</div>
+					</Fade>
+
 				</div>
 			</div>
 		</CSSTransition>
