7cc41bc6f002c16980a87353ef5f1d5f0dc9e2a4
Devin Rucks <blindnives@gmail.com>
2020-04-25 13:24:20 -0400

    Added basic layout for hamburger menu when screen width is less than 560px

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import HamburgerMenu from 'react-hamburger-menu'
+import useWindowDimensions from './WindowDimension'
 import {
      AiOutlineEye, AiOutlineHome,
      AiOutlineGithub, AiOutlineContainer,
@@ -9,8 +11,10 @@ import '../styling/navbar.scss'
 
 const Navbar = (props) => {
      const [active, setActive] = useState('')
+     const [menuOpen, setMenuOpen] = useState(false)
+     const { width } = useWindowDimensions();
 
-     const handleClick = (value) => {
+     const handleLinkClick = (value) => {
           setActive(value)
      }
 
@@ -18,35 +22,87 @@ const Navbar = (props) => {
           return value === active ? true : false
      }
 
+     const handleMenuClick = () => {
+          setMenuOpen(!menuOpen)
+          if (menuOpen) console.log("menu open")
+     }
+
      return (
-          <div id="navbar-container">
+          <>
+               <div id="navbar-container" style={{ justifyContent: width < 560 ? "flex-end" : "center" }}>
 
-               <section id="main-links-container">
-                    <Link id="home-nav" className="link" onClick={() => handleClick('home')} to="/">
-                         <AiOutlineHome color={isActive('home') ? "#08fdd8" : "#555555"} size={25} />
-                    </Link>
+                    <section id="main-links-container">
+                         <Link id="home-nav" className="link" onClick={() => handleLinkClick('home')} to="/">
+                              <AiOutlineHome color={isActive('home') ? "#08fdd8" : "#555555"} size={25} />
+                         </Link>
 
-                    <Link id="my-work-nav" className="link" onClick={() => handleClick('project')} to="/projects">
-                         <AiOutlineEye color={isActive('project') ? "#08fdd8" : "#555555"} size={30} />
-                    </Link>
+                         <Link id="my-work-nav" className="link" onClick={() => handleLinkClick('project')} to="/projects">
+                              <AiOutlineEye color={isActive('project') ? "#08fdd8" : "#555555"} size={30} />
+                         </Link>
 
-                    <Link id="contact-nav" className="link" onClick={() => handleClick('contact')} to="/contact">
-                         <AiOutlineMail color={isActive('contact') ? "#08fdd8" : "#555555"} size={25} />
-                    </Link>
-               </section>
+                         <Link id="contact-nav" className="link" onClick={() => handleLinkClick('contact')} to="/contact">
+                              <AiOutlineMail color={isActive('contact') ? "#08fdd8" : "#555555"} size={25} />
+                         </Link>
+                    </section>
 
-               <section id="extra-links-container">
-                    <div id="github-nav" className="extra-link">
-                         <AiOutlineGithub color={"#555555"} size={18} />
+                    <section id="extra-links-container">
+                         <div id="github-nav" className="extra-link">
+                              <AiOutlineGithub color={"#555555"} size={18} />
+                         </div>
+                         <div id="resume-nav" className="extra-link">
+                              <AiOutlineContainer color={"#555555"} size={18} />
+                         </div>
+                         <div id="linkedin-nav" className="extra-link">
+                              <AiOutlineLinkedin color={"#555555"} size={18} />
+                         </div>
+                    </section>
+
+                    {width < 560 &&
+                         <section id="hamburger-menu-container">
+                              <HamburgerMenu
+                                   isOpen={menuOpen}
+                                   menuClicked={handleMenuClick}
+                                   strokeWidth={3}
+                                   color="#c9c9c9"
+                                   height={25}
+                                   width={40}
+                                   animationDuration={.25}
+                              />
+                         </section>
+                    }
+               </div>
+               {menuOpen &&
+                    <div id="slide-menu-container">
+                         <section id="slide-menu-link-container">
+                              <Link className="slide-menu-link" onClick={() => handleLinkClick('home')} to="/">
+                                   <AiOutlineHome color={isActive('home') ? "#08fdd8" : "#555555"} size={30} />
+                              </Link>
+
+                              <Link className="slide-menu-link" onClick={() => handleLinkClick('project')} to="/projects">
+                                   <AiOutlineEye color={isActive('project') ? "#08fdd8" : "#555555"} size={30} />
+                              </Link>
+
+                              <Link className="slide-menu-link" onClick={() => handleLinkClick('contact')} to="/contact">
+                                   <AiOutlineMail color={isActive('contact') ? "#08fdd8" : "#555555"} size={30} />
+                              </Link>
+                         </section>
+
+                         <hr className="slide-menu-separator" />
+
+                         <section id="slide-menu-link-container">
+                              <div className="slide-menu-link">
+                                   <AiOutlineGithub color={"#555555"} size={30} />
+                              </div>
+                              <div className="slide-menu-link">
+                                   <AiOutlineContainer color={"#555555"} size={30} />
+                              </div>
+                              <div className="slide-menu-link">
+                                   <AiOutlineLinkedin color={"#555555"} size={30} />
+                              </div>
+                         </section>
                     </div>
-                    <div id="resume-nav" className="extra-link">
-                         <AiOutlineContainer color={"#555555"} size={18} />
-                    </div>
-                    <div id="linkedin-nav" className="extra-link">
-                         <AiOutlineLinkedin color={"#555555"} size={18} />
-                    </div>
-               </section>
-          </div>
+               }
+          </>
      )
 }
 
